282b84a2ce6ac51bcb77422cc6814c88f5c2b34b
Crone1 <34443838+Crone1@users.noreply.github.com>
2018-06-19 11:57:48 +0100

    Add files via upload

--- Lotto_Matches_Calculator.py
+++ Lotto_Matches_Calculator.py
@@ -1,62 +1,47 @@
 import sys
 import random
 
-itterations = 100000000
+
+def put_in_commas(num):
+    if len(num) < 4:
+        return num
+
+    return str(put_in_commas(num[:-3])) + ',' + str(num[-3:])
+
+print('\nHow many lotto simulations do you want to take place? (Input Below)')
+
+itterations = int(input())
+
+print('\nFrom a simulation of {} Lotto Draws, You got:'.format(
+    put_in_commas(str(itterations))))
+
 matches = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
 numbers = sys.argv[1:]
-lucky_nums = []
+lucky_nums = set()
 
 for number in numbers:
-    lucky_nums.append(int(number))
-
-numbers = set(lucky_nums)
+    lucky_nums.add(int(number))
 
 for i in range(itterations):
-    lotto_numbers = random.sample(range(1,48), 6)
+    lotto_numbers = set(random.sample(range(1, 48), 6))
 
-    draw = set(lotto_numbers)
-
-    match = draw & numbers
+    match = lotto_numbers & lucky_nums
 
     if len(match) > 1:
         matches[len(match)] += 1
 
-try:
-    prob_6 = str(itterations // matches[6])
-    if len(prob_6) == 8:
-        prob_6 = prob_6[:2] + ',' + prob_6[2:5] + ',' + prob_6[5:]
+prob = []
+new_matches = []
+for i in range(2, 7):
+    try:
+        prob.append(put_in_commas(str(itterations // matches[i])))
 
-    if len(prob_6) == 7:
-        prob_6 = prob_6[0] + ',' + prob_6[1:4] + ',' + prob_6[4:]
+    except ZeroDivisionError:
+        prob.append('?')
 
-    elif len(prob_6) == 6:
-        prob_6 =  prob_6[0:3] + ',' + prob_6[3:]
+    finally:
+        new_matches.append(put_in_commas(str(matches[i])))
 
-except ZeroDivisionError:
-    prob_6 = '?'
-
-try:
-    prob_5 = str(itterations // matches[5])
-    if len(prob_5) == 7:
-        prob_5 = prob_5[0] + ',' + prob_5[1:4] + ',' + prob_5[4:]
-
-    elif len(prob_5) == 6:
-        prob_5 =  prob_5[0:3] + ',' + prob_5[3:]
-
-    elif len(prob_5) == 5:
-        prob_5 =  prob_5[0:2] + ',' + prob_5[2:]
-
-except ZeroDivisionError:
-    prob_5 = '?'
-
-print('Number of times you got 2 Matches : {:>{}} => You have a 1 in {} chance'.format(
-    matches[2], len(str(matches[2])), itterations // matches[2]))
-print('Number of times you got 3 Matches : {:>{}} => You have a 1 in {} chance'.format(
-    matches[3], len(str(matches[2])), itterations // matches[3]))
-print('Number of times you got 4 Matches : {:>{}} => You have a 1 in {} chance'.format(
-    matches[4], len(str(matches[2])), itterations // matches[4]))
-print('Number of times you got 5 Matches : {:>{}} => You have a 1 in {} chance'.format(
-    matches[5], len(str(matches[2])), prob_5))
-print('Number of times you got 6 Matches : {:>{}} => You have a 1 in {} chance'.format(
-    matches[6], len(str(matches[2])), prob_6))
+        print('   {} Matches {:>{}} times => You have a 1 in {} chance of winning'.format(
+            i, new_matches[i - 2], len(new_matches[0]), prob[i - 2]))
