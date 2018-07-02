67545e082ded5b36c8a1abdf51f02c10803456b8
Crone1 <34443838+Crone1@users.noreply.github.com>
2018-07-02 19:33:15 +0100

    Add files via upload

--- Quadratic_equation_solver(py3).py
+++ Quadratic_equation_solver(py3).py
@@ -1,104 +1,173 @@
-#!usr/bin/env python
+#!usr/bin/env python3
 
 import sys
 
 
-def minus_b_formula(in_sqr_root):
-    equation = []
-    sqr_root = (int(in_sqr_root)) ** (0.5)
+def set_up():
+    print('What is your variable name?  (only one letter)')
+    org_x = input()
+    x = org_x.lower()
 
-    equation.append(str((-b + sqr_root) / (2 * a)))
+    print('\nWhat is your equation?')
+    print('(Example equation:  x**2 + x - 6 = 0)\n')
+    s = sys.stdin.readline().strip().lower()
+    return (x, s)
 
-    equation.append(str((-b - sqr_root) / (2 * a)))
 
-    return equation
+def find_a(s, i, m):
+    if s[:i] == '':
+        return 1
 
-
-print('What is Your Variable Name?')
-x = input()
-
-print('\nWhat is Your Equation?')
-print('(Example equation:  x**2 + x - 6 = 0)\n')
-s = sys.stdin.readline().strip()
-
-a = ''
-b = ''
-c = ''
-
-# RULES:
-# print '(Use Two Asctriscs To Indicate To The Power Of)'
-# variable name can only be one letter
-# solution must not be complex
-
-i = 0
-while i < len(s) and s[i] != x:
-    i = i + 1
-
-if s[i + 3] == '2':
-
-    if i < len(s):
+    else:
         try:
-            a = float(int(s[:i]))
+            return float(s[:i])
 
         except (ValueError,  SyntaxError):
-            a = 1
+            if m == 4:
+                print(
+                    '\n!!!!   Error at the values before the "{}**2"   !!!!'.format(org_x))
+
+            elif m == 3:
+                print(
+                    '\n!!!!   Error at the values before the "{}^2"   !!!!'.format(org_x))
+
+        sys.exit(0)
+
+
+def find_b(s, i, j, m):
+
+    chars = ''.join([c for c in s[i + m:j] if c != ' '])
+
+    if chars == '+':
+        return 1
+
+    elif chars == '-':
+        return -1
+
+    if chars[0] == '+' or chars[0] == '-':
+        try:
+            return float(chars[1:])
+
+        except (ValueError,  SyntaxError):
+            print('\n!!!!   Error at the values before the "{}"   !!!!'.format(org_x))
+
+            sys.exit(0)
+
+
+def find_c_no_eq(s, l, j):
+
+    chars = ''.join([c for c in s[j + 1:l] if c != ' '])
+
+    if s[j + 1:l] == '' or len(chars) == 0:
+        return 0
+
+    if chars[0] == '+' and chars[0] == '-':
+        try:
+            return float(chars[1:])
+
+        except (ValueError,  SyntaxError):
+            print(
+                '\n!!!!   Error at the values just after the "{}", just before the end   !!!!'.format(org_x))
+
+            sys.exit(0)
+
+
+def find_c_with_eq(s, l, j):    # deals with equals
+
+    c_left = find_c_no_eq(s, l, j)
+
+    if s[l + 1] == '=':
+        n = 2
+
+    else:
+        n = 1
+
+    chars = ''.join([c for c in s[l + n:] if c != ' '])
+
+    try:
+        return c_left - float(chars)
+
+    except (ValueError,  SyntaxError):
+        if n == 2:
+            print('\n!!!!   Error at the values after the "==", just at the end   !!!!')
+
+        elif n == 1:
+            print('\n!!!!   Error at the values after the "=", just at the end   !!!!')
+
+        sys.exit(0)
+
+
+def get_a_b_c(x, s):
+    i = 0
+    while i < len(s) and s[i] != x:  # find first x
+        i = i + 1
+
+    if i < len(s):
+
+        if s[i:i + 4] == '{}**2'.format(x):
+            m = 4
+
+        elif s[i:i + 3] == '{}^2'.format(x):
+            m = 3
+
+        else:
+            print('\n!!!!   Error, This is not a quadratic equation  !!!!')
+            sys.exit(0)
+
+        a = find_a(s, i, m)  # get value for a
 
         j = i + 1
-        while j < len(s) and s[j] != x:
+        while j < len(s) and s[j] != x:  # find second x
             j = j + 1
 
         if j < len(s):
-            try:
-                b = float(int(s[i + 4:j]))
 
-            except (ValueError,  SyntaxError):
-                b = 1
+            b = find_b(s, i, j, m)  # get value for b
 
             l = j + 1
-            while l < len(s) and s[l] != '=':
-                l = l + 1
 
-            if l == len(s):
-                try:
-                    c = float(int(s[j + 1:l]))
-
-                except (ValueError,  SyntaxError):
-                    c = 0
-
-            elif l < len(s):
-                c = float(int(s[j + 1:l])) - float(int(s[l + 1:]))
-
-        elif j == len(s):
+        else:
             b = 0
-            try:
-                c = float(int(s[i + 4:]))
+            l = i + m
 
-            except (ValueError,  SyntaxError):
-                c = 0
+        while l < len(s) and s[l] != '=':  # find equals sign
+            l = l + 1
 
-        else:
-            print('\n!!!!   Error, This is not a Quadratic   !!!!')
+        if l == len(s):
+            c = find_c_no_eq(s, l, j)  # get value for c
 
-    else:
-        print('\n!!!!   Error, Wrong Variable Name Given   !!!!')
-
-else:
-    print('\n!!!!   Error, This is not a Quadratic   !!!!')
-
-
-if (a or a == 0) and (b or b == 0) and (c or c == 0):
-
-    in_sqr_root = (b ** 2) - (4 * a * c)
-
-    if in_sqr_root >= 0:
-        [equation_plus, equation_minus] = minus_b_formula(in_sqr_root)
-
-        if equation_minus != equation_plus:
-            print('{} = {} AND {} = {}'.format(
-                x, equation_plus, x, equation_minus))
-
-        else:
-            print('{} = {}'.format(x, equation_plus))
+        elif l < len(s):
+            c = find_c_with_eq(s, l, j)
 
     else:
-        print('\n!!!!   Error, This Equation has a Complex Solution   !!!!')
+        print('\n!!!!   Error, Wrong variable name given   !!!!')
+        sys.exit(0)
+
+    return (a, b, c)
+
+
+def get_and_print_solution_quadratic(x, a, b, c):
+    in_sqr_root = (b ** 2) - (4 * a * c)    #minus b formula
+    sqr_root = (int(in_sqr_root)) ** (0.5)
+
+    equation_plus = str((-b + sqr_root) / (2 * a))
+    equation_minus = str((-b - sqr_root) / (2 * a))
+
+    if equation_minus != equation_plus:
+        print('{} = {} AND {} = {}'.format(
+            org_x, equation_plus, org_x, equation_minus))
+
+    else:
+        print('{} = {}'.format(org_x, equation_plus))
+
+
+def main():
+    # RULES:
+    # variable name can only be one letter
+
+    x, s = set_up()
+    a, b, c = get_a_b_c(x, s)
+    get_and_print_solution_quadratic(x, a, b, c)
+
+if __name__ == '__main__':
+    main()
