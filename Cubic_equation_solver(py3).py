d6659eb36431b9de81b1c0c540989d79f844eb04
Crone1 <34443838+Crone1@users.noreply.github.com>
2018-09-19 21:33:21 +0100

    Add files via upload

--- Cubic_equation_solver(py3).py
+++ Cubic_equation_solver(py3).py
@@ -17,7 +17,7 @@ def set_up():
 
     print('\nWhat is your equation?')
     print(
-        '(Example equation:  x**3 + x**2 + x - 6 = 4)   [Automatically put equal to zero]\n')
+        '(Example equation:  x^3 + x^2 + x - 6 = 4)   [Automatically put equal to zero]\n')
     s = sys.stdin.readline().strip().lower()
     return (org_x, x, s)
 
@@ -40,7 +40,7 @@ def find_a(org_x, s, i, m):
                 print(
                     '\n!!!!   Error at the values before the "{}^3"   !!!!'.format(org_x))
 
-        sys.exit(0)
+            sys.exit(0)
 
 
 def find_b(org_x, s, i, j, m, n):
@@ -103,7 +103,7 @@ def find_d_no_eq(org_x, s, k, l):
 
         except (ValueError,  SyntaxError):
             print(
-                '\n!!!!   Error at the values just after the "{}", just before the end   !!!!'.format(org_x))
+                '\n!!!!   Error at the values after the "{}", just before the end   !!!!'.format(org_x))
 
             sys.exit(0)
 
@@ -158,7 +158,7 @@ def get_a_b_c_d(org_x, x, s):
         a = find_a(org_x, s, i, m)  # get value for a
 
         #----------------------B-------------------------------
-        j = i + 1
+        j = i + m
         while j < len(s) and s[j] != x:  # find second x
             j = j + 1
 
@@ -166,9 +166,13 @@ def get_a_b_c_d(org_x, x, s):
 
             if s[j:j + 4] == '{}**2'.format(x):
                 n = 4
+                b = find_b(org_x, s, i, j, m, n)  # get value for b
+                l = j + n
 
             elif s[j:j + 3] == '{}^2'.format(x):
                 n = 3
+                b = find_b(org_x, s, i, j, m, n)  # get value for b
+                l = j + n
 
             else:
                 b = 0
@@ -177,10 +181,8 @@ def get_a_b_c_d(org_x, x, s):
             #   'j' is the position of the second x
             #   'j + n' is the position after the 2
 
-            b = find_b(org_x, s, i, j, m, n)  # get value for b
-
             #--------------------C-------------------------------
-            l = j + 1
+
             while l < len(s) and s[l] != x:  # find third x
                 l = l + 1
 
@@ -194,18 +196,39 @@ def get_a_b_c_d(org_x, x, s):
 
             else:
                 c = 0
-                k = j + n
+
+                if b != 0:
+                    k = j + n
+
+                else:
+                    k = i + m
+
+        else:
+            b = 0
+            c = 0
+            k = i + m
 
             #--------------------D---------------------------------
 
         while k < len(s) and s[k] != '=':  # find equals sign
             k = k + 1
 
+        if c != 0:
+            # l remains the same 'l = l'
+            pass
+
+        else:
+            if b != 0:
+                l = j + n - 1
+
+            else:
+                l = i + m - 1
+
         if k == len(s):
-            d = find_d_no_eq(org_x, s, k, l)  # get value for c
+            d = find_d_no_eq(org_x, s, k, l)  # get value for d
 
         elif k < len(s):
-            d = find_d_with_eq(org_x, s, k, l)
+            d = find_d_with_eq(org_x, s, k, l)  # get value for d
 
     else:
         print('\n!!!!   Error, Wrong variable name given   !!!!')
@@ -226,52 +249,132 @@ def get_roots(a, b, c, d):
 
     big_sqr_minus = ((one - small_sqr) / 2)**(1 / 3)
 
-    guage = (18 * a * b * c * d) - (4 * d * (b**3)) + \
+    polynomial_discriminant = (18 * a * b * c * d) - (4 * d * (b**3)) + \
         ((b**2) * (c**2)) - (4 * a * (c**3)) - (27 * (a**2) * (d**2))
 
-    print(guage)
+    pos_cube_root_of_unity = complex((-1 / 2) + (1 / 2j * (3)**(1 / 2)))
 
-    if guage < 0:
-        # 1 real route + 2 complex conjugates
+    neg_cube_root_of_unity = complex((-1 / 2) - (1 / 2j * (3)**(1 / 2)))
 
-        root_1 = '{:.4f}'.format(-(1 / (3 * a)) *
-                                 (b + big_sqr_plus + (zero / big_sqr_plus)))
+    if polynomial_discriminant == 0:
 
-        root_2 = '{:.4f}'.format(-(1 / (3 * a)) * (b + (-1 / 2 + (
-            1 / 2 * complex((3)**(1 / 2)))) * big_sqr_plus + (zero / big_sqr_plus)))
+        if zero == 0:
+            # One triple root
+            root_1 = root_2 = root_3 = -b / (3 * a)
 
-        root_3 = '{:.4f}'.format(-(1 / (3 * a)) * (b + (-1 / 2 - (
-            1 / 2 * complex((3)**(1 / 2)))) * big_sqr_plus + (zero / big_sqr_plus)))
+        else:
+            # One double root and a single root
+            root_1 = root_2 = ((9 * a * d) - (b * c)) / (2 * zero)
 
-        root_2.isconjegate(root_3)
+            root_3 = ((4 * a * b * c) - (9 * d * (a**2)) - b**3) / (a * zero)
+
+    else:
+        # 1 real root + 2 complex conjugates
+        # OR 3 disict real routes
+        if zero == 0 and one + small_sqr == 0:
+            root_1 = -(1 / (3 * a)) * (
+                b + big_sqr_minus + (zero / big_sqr_minus))
+
+            root_2 = -(1 / (3 * a)) * (
+                b + (pos_cube_root_of_unity * big_sqr_minus) + (zero / big_sqr_minus))
+
+            root_3 = -(1 / (3 * a)) * (
+                b + (neg_cube_root_of_unity * big_sqr_minus) + (zero / big_sqr_minus))
+
+        elif (zero == 0 and one - small_sqr == 0) or zero != 0:
+
+            root_1 = -(1 / (3 * a)) * (
+                b + big_sqr_plus + (zero / big_sqr_plus))
+
+            root_2 = -(1 / (3 * a)) * (
+                b + (pos_cube_root_of_unity * big_sqr_plus) + (zero / (pos_cube_root_of_unity * big_sqr_plus)))
+
+            root_3 = -(1 / (3 * a)) * (
+                b + (neg_cube_root_of_unity * big_sqr_plus) + (zero / (neg_cube_root_of_unity * big_sqr_plus)))
 
     return (root_1, root_2, root_3)
 
-    if guage == 0:
 
-        if zero == 0:
+def turn_to_string(l):
 
-            root_1 = root_2 = root_3 = '{:.4f}'.format(-b / (3 * a))
+    n_l = []
+    for r in l:
+
+        if r.imag == 0:
+            n_l.append('{:.4f}'.format(r.real))
+
+        elif r.real == 0:
+            n_l.append('{:.4f}'.format(r.imag))
 
         else:
-            root_1 = root_2 = '{:.4f}'.format(
-                ((9 * a * d) - (b * c)) / (2 * zero))
+            n_l.append('{:.4f}'.format(r))
 
-            root_3 = '{:.4f}'.format(
-                ((4 * a * b * c) - (9 * d * (a**2)) - b**3) / (a * zero))
-
-        return (root_1, root_2, root_3)
-
-    if guage > 0:
-        # roots = 3 real
-        if zero != 0:
-        root_1 = '{:.4f}'.format(-(1 / (3 * a)) *
-                                 (b + big_sqr_plus + (zero / big_sqr_plus)))
+    return n_l
 
 
 def print_solution(org_x, root_1, root_2, root_3):
 
-    roots = set([root_1, root_2, root_3])
+    # all real
+    # all complex
+    # half and half
+
+    l = [root_1, root_2, root_3]
+    strings = []
+    comp = []
+
+    for r in l:
+
+        a = str(r)
+
+        j = 0
+        while j < len(a) and a[j] != '.':
+            j = j + 1
+
+        if not isinstance(r, complex):
+
+            if j + 4 < len(a):
+                strings.append('{:.4f}'.format(r))
+
+            else:
+                strings.append(r)
+
+        else:
+            comp.append(r)
+
+    print('L_B = {}'.format(l))
+
+    if comp:
+
+        n_l = turn_to_string(comp)
+
+        print('L_D = {}'.format(n_l))
+
+        for r in n_l:
+
+            comp_2.append(complex(r))
+            i = i + 1
+
+        print('Comp_2 = {}'.format(comp_2))
+        print('i = {}'.format(i))
+
+        for j in range(0, i):
+
+            if comp_2[j].imag == 0:
+
+                new = turn_to_string(comp_2)
+
+            else:
+                new = comp_2
+
+        if new:
+
+            for e in new:
+
+                final.append(e)
+
+    print('L_A = {}'.format(final))
+
+    roots = set(final)
     root_list = []
 
     for r in roots:
