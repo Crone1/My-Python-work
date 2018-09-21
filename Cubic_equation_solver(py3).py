a336f42ccdbab7130aa227d485804106971a7319
Crone1 <34443838+Crone1@users.noreply.github.com>
2018-09-21 20:17:56 +0100

    Add files via upload

--- Cubic_equation_solver(py3).py
+++ Cubic_equation_solver(py3).py
@@ -43,9 +43,9 @@ def find_a(org_x, s, i, m):
             sys.exit(0)
 
 
-def find_b(org_x, s, i, j, m, n):
+def find_b(org_x, s, j, j_start, n):
 
-    chars = ''.join([c for c in s[i + m:j] if c != ' '])
+    chars = ''.join([c for c in s[j_start:j] if c != ' '])
 
     if chars == '+':
         return 1
@@ -69,6 +69,17 @@ def find_b(org_x, s, i, j, m, n):
 
             sys.exit(0)
 
+    else:
+        if n == 4:
+            print(
+                '\n!!!!   Error at the values before the "{}**2"   !!!!'.format(org_x))
+
+        elif n == 3:
+            print(
+                '\n!!!!   Error at the values before the "{}^2"   !!!!'.format(org_x))
+
+        sys.exit(0)
+
 
 def find_c(org_x, s, j, l, n):
 
@@ -89,6 +100,11 @@ def find_c(org_x, s, j, l, n):
 
             sys.exit(0)
 
+    else:
+        print('\n!!!!   Error at the values before the "{}"   !!!!'.format(org_x))
+
+        sys.exit(0)
+
 
 def find_d_no_eq(org_x, s, k, l):
 
@@ -107,37 +123,56 @@ def find_d_no_eq(org_x, s, k, l):
 
             sys.exit(0)
 
+    else:
+        print(
+            '\n!!!!   Error at the values after the "{}", just before the end   !!!!'.format(org_x))
+
+        sys.exit(0)
+
 
 def find_d_with_eq(org_x, s, k, l):    # deals with equals
 
     d_left = find_d_no_eq(org_x, s, k, l)
 
-    if s[k + 1] == '=':
-        p = 2
+    if k == len(s) or (k + 1 == len(s) and s[k + 1] == '='):
+
+        if s[k + 1] == '=':
+            p = 2
+
+        else:
+            p = 1
+
+        chars = ''.join([c for c in s[k + p:] if c != ' '])
+
+        if len(chars) == 0:
+            return d_left
+
+        else:
+
+            try:
+                return d_left - float(chars)
+
+            except (ValueError,  SyntaxError):
+                if p == 2:
+                    print(
+                        '\n!!!!   Error at the values after the "==", just at the end   !!!!')
+
+                elif p == 1:
+                    print(
+                        '\n!!!!   Error at the values after the "=", just at the end   !!!!')
+
+                sys.exit(0)
 
     else:
-        p = 1
-
-    chars = ''.join([c for c in s[k + p:] if c != ' '])
-
-    try:
-        return float(d_left) - float(chars)
-
-    except (ValueError,  SyntaxError):
-        if p == 2:
-            print('\n!!!!   Error at the values after the "==", just at the end   !!!!')
-
-        elif p == 1:
-            print('\n!!!!   Error at the values after the "=", just at the end   !!!!')
-
-        sys.exit(0)
+        return d_left
 
 
 def get_a_b_c_d(org_x, x, s):
 
     #------------------------A--------------------------------
     i = 0
-    while i < len(s) and s[i] != x:  # find first x
+    # find first x
+    while i < len(s) and s[i:i + 4] != '{}**3'.format(x) and s[i:i + 3] != '{}^3'.format(x):
         i = i + 1
 
     if i < len(s):
@@ -155,74 +190,55 @@ def get_a_b_c_d(org_x, x, s):
         #   'i' is the position of the first x
         #   'i + m' is the position after the 3
 
-        a = find_a(org_x, s, i, m)  # get value for a
+        a = find_a(org_x, s, i, m)
 
         #----------------------B-------------------------------
-        j = i + m
-        while j < len(s) and s[j] != x:  # find second x
+        j = j_start = i + m
+        while j < len(s) and s[j:j + 4] != '{}**2'.format(x) and s[j:j + 3] != '{}^2'.format(x):
             j = j + 1
 
         if j < len(s):
 
             if s[j:j + 4] == '{}**2'.format(x):
                 n = 4
-                b = find_b(org_x, s, i, j, m, n)  # get value for b
-                l = j + n
 
             elif s[j:j + 3] == '{}^2'.format(x):
                 n = 3
-                b = find_b(org_x, s, i, j, m, n)  # get value for b
-                l = j + n
 
             else:
                 b = 0
-                l = i + m
+                j = j_start
+                n = 0
+
+            if n:
+                b = find_b(org_x, s, j, j_start, n)
 
             #   'j' is the position of the second x
-            #   'j + n' is the position after the 2
+            #   'j + n' is the position after the **2/^2
 
             #--------------------C-------------------------------
-
+            l = l_start = j + n
             while l < len(s) and s[l] != x:  # find third x
                 l = l + 1
 
             if l < len(s):
-
                 #   'l' is the position of the third x
 
                 c = find_c(org_x, s, j, l, n)  # get value for c
 
-                k = l + 1
-
             else:
                 c = 0
-
-                if b != 0:
-                    k = j + n
-
-                else:
-                    k = i + m
+                l = l_start - 1
 
         else:
             b = 0
             c = 0
-            k = i + m
+            l = j_start - 1
 
             #--------------------D---------------------------------
-
+        k = l + 1
         while k < len(s) and s[k] != '=':  # find equals sign
             k = k + 1
-
-        if c != 0:
-            # l remains the same 'l = l'
-            pass
-
-        else:
-            if b != 0:
-                l = j + n - 1
-
-            else:
-                l = i + m - 1
 
         if k == len(s):
             d = find_d_no_eq(org_x, s, k, l)  # get value for d
@@ -259,19 +275,19 @@ def get_roots(a, b, c, d):
     if polynomial_discriminant == 0:
 
         if zero == 0:
-            # One triple root
+            # One triple root                      ---- x**3 - 3x**2 + 3x - 1
             root_1 = root_2 = root_3 = -b / (3 * a)
 
         else:
-            # One double root and a single root
+            # One double root and a single root     ---- x**3 + x**2 - 33x + 63
             root_1 = root_2 = ((9 * a * d) - (b * c)) / (2 * zero)
 
             root_3 = ((4 * a * b * c) - (9 * d * (a**2)) - b**3) / (a * zero)
 
     else:
-        # 1 real root + 2 complex conjugates
-        # OR 3 disict real routes
-        if zero == 0 and one + small_sqr == 0:
+        # 1 real root + 2 complex conjugates        ---- x**3 + x**2 + x + 1
+        # OR 3 disict real routes                   ---- x**3 - 6x**2 + 11x - 6
+        if one + small_sqr == 0:
             root_1 = -(1 / (3 * a)) * (
                 b + big_sqr_minus + (zero / big_sqr_minus))
 
@@ -281,7 +297,7 @@ def get_roots(a, b, c, d):
             root_3 = -(1 / (3 * a)) * (
                 b + (neg_cube_root_of_unity * big_sqr_minus) + (zero / big_sqr_minus))
 
-        elif (zero == 0 and one - small_sqr == 0) or zero != 0:
+        else:
 
             root_1 = -(1 / (3 * a)) * (
                 b + big_sqr_plus + (zero / big_sqr_plus))
@@ -292,120 +308,148 @@ def get_roots(a, b, c, d):
             root_3 = -(1 / (3 * a)) * (
                 b + (neg_cube_root_of_unity * big_sqr_plus) + (zero / (neg_cube_root_of_unity * big_sqr_plus)))
 
-    return (root_1, root_2, root_3)
+    return [root_1, root_2, root_3]
 
 
-def turn_to_string(l):
+def strip_string(s):
 
-    n_l = []
-    for r in l:
+    t = s.rstrip('0').rstrip('.')
+    if t == '':
+        return '0'
 
-        if r.imag == 0:
-            n_l.append('{:.4f}'.format(r.real))
-
-        elif r.real == 0:
-            n_l.append('{:.4f}'.format(r.imag))
-
-        else:
-            n_l.append('{:.4f}'.format(r))
-
-    return n_l
+    else:
+        return t
 
 
-def print_solution(org_x, root_1, root_2, root_3):
+def tidy_roots(l):
 
-    # all real
-    # all complex
-    # half and half
-
-    l = [root_1, root_2, root_3]
     strings = []
-    comp = []
 
     for r in l:
-
-        a = str(r)
-
-        j = 0
-        while j < len(a) and a[j] != '.':
-            j = j + 1
+        comp = []
 
         if not isinstance(r, complex):
 
+            a = str(r)
+
+            j = 0
+            while j < len(a) and a[j] != '.':
+                j = j + 1
+
             if j + 4 < len(a):
-                strings.append('{:.4f}'.format(r))
+                strings.append(strip_string('{:.4f}'.format(r)))
 
             else:
-                strings.append(r)
+                strings.append(strip_string(a))
 
         else:
-            comp.append(r)
+            if r.real != 0:
+                a = str(r.real)
 
-    print('L_B = {}'.format(l))
+                j = 0
+                while j < len(a) and a[j] != '.':
+                    j = j + 1
 
-    if comp:
+                if j + 4 < len(a):
+                    if strip_string('{:.4f}'.format(r.real)) != '0' and strip_string('{:.4f}'.format(r.real)) != '-0':
+                        comp.append(strip_string('{:.4f}'.format(r.real)))
 
-        n_l = turn_to_string(comp)
+                else:
+                    comp.append(strip_string(a))
 
-        print('L_D = {}'.format(n_l))
+            if r.imag != 0:
 
-        for r in n_l:
+                b = str(abs(r.imag))
 
-            comp_2.append(complex(r))
-            i = i + 1
+                i = 0
+                while i < len(b) and b[i] != '.':
+                    i = i + 1
 
-        print('Comp_2 = {}'.format(comp_2))
-        print('i = {}'.format(i))
+                if i + 4 < len(b):
+                    if strip_string('{:.4f}'.format(r.imag)) != '0' and strip_string('{:.4f}'.format(r.imag)) != '-0':
+                        if r.imag > 0:
+                            comp.append(strip_string(
+                                '+ {:.4f}'.format(abs(r.imag))))
 
-        for j in range(0, i):
+                        elif r.imag < 0:
+                            comp.append(strip_string(
+                                '- {:.4f}'.format(abs(r.imag))))
 
-            if comp_2[j].imag == 0:
+                else:
+                    if r.imag > 0:
+                        comp.append('+ ' + strip_string(b))
 
-                new = turn_to_string(comp_2)
+                    elif r.imag < 0:
+                        comp.append('- ' + strip_string(b))
+
+            if len(comp) == 2:
+                strings.append('{} {}j'.format(comp[0], comp[1]))
+
+            elif len(comp) == 1:
+                if strip_string('{:.4f}'.format(r.real)) == '0' or strip_string('{:.4f}'.format(r.real)) == '-0':
+                    strings.append('{}j'.format(comp[0]))
+
+                else:
+                    strings.append(str(comp[0]))
+
+            elif len(comp) == 0:
+                strings.append('0')
+
+    return strings
+
+
+def sign(n):
+    return n >= 0
+
+
+def print_solution(org_x, root_list, a):
+
+    new = []
+    for r in root_list:
+        try:
+            if sign(int(r)):
+                new.append('- {}'.format(abs(int(r))))
 
             else:
-                new = comp_2
+                new.append('+ {}'.format(abs(int(r))))
 
-        if new:
+        except ValueError:
+            break
 
-            for e in new:
+    if len(new) == len(root_list):
+        if a == 1:
 
-                final.append(e)
+            print('\nThe factors are:')
+            print('({} {})({} {})({} {})'.format(org_x, new[
+                  0], org_x, new[1], org_x, new[2]))
 
-    print('L_A = {}'.format(final))
+    print('\nThe roots are approximately:')
 
-    roots = set(final)
-    root_list = []
+    no_duplicute_roots = list(set(root_list))
 
-    for r in roots:
-        j = r.rstrip('0').rstrip('.')
-        if j == '':
-            root_list.append('0')
-
-        root_list.append(j)
-
-    if len(root_list) == 3:
+    if len(no_duplicute_roots) == 3:
         print('{} = {} AND {} = {} AND {} = {}'.format(
-            org_x, root_list[0], org_x, root_list[1], org_x, root_list[2]))
+            org_x, no_duplicute_roots[0], org_x, no_duplicute_roots[1], org_x, no_duplicute_roots[2]))
 
-    elif len(root_list) == 1:
-        print('{} = {}'.format(org_x, root_list[0]))
-
-    elif len(root_list) == 2:
+    elif len(no_duplicute_roots) == 2:
         print('{} = {} AND {} = {}'.format(
-            org_x, root_list[0], org_x, root_list[1]))
+            org_x, no_duplicute_roots[0], org_x, no_duplicute_roots[1]))
+
+    elif len(no_duplicute_roots) == 1:
+        print('{} = {}'.format(org_x, no_duplicute_roots[0]))
 
 
 def main():
 
     org_x, x, s = set_up()
+
     a, b, c, d = get_a_b_c_d(org_x, x, s)
 
-    #print('A = {}, B = {}, C = {}, D = {}'.format(a, b, c, d))
+    list_of_roots = get_roots(a, b, c, d)
 
-    (r1, r2, r3) = get_roots(a, b, c, d)
+    tidied_roots = tidy_roots(list_of_roots)
 
-    print_solution(org_x, r1, r2, r3)
+    print_solution(org_x, tidied_roots, a)
 
 if __name__ == '__main__':
     main()
